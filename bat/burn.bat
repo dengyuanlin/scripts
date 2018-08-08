2bf485514382028a0a96b2d35bc9808d5dade3c1
dengyuanlin <34824313+dengyuanlin@users.noreply.github.com>
2018-08-08 22:32:58 +0800

    Update burn.bat

--- bat/burn.bat
+++ bat/burn.bat
@@ -1,3 +1,25 @@
+::clear screen
+cls
+::to use !varname!
+setlocal enabledelayedexpansion
+
+set CurrentPATH=%~dp0
+set LOGFILE=%CurrentPATH%path.log
+set DESTDIR=%CurrentPATH%tempdir
+
 @echo off
 
-@adb reboot boot loader
+::@adb reboot bootloader
+
+cd /d %CurrentPATH%
+
+mkdir %DESTDIR%
+::rem can also comment
+rem use rd/del to delete dir
+rd /s /q %DESTDIR%
+del /s /q %DESTDIR%
+
+set comport=com5
+set /p comport=please input the port:
+
+echo port=%comport% > ./%LOGFILE%
